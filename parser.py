86fddf39185f2e0642ca3b2bcf6eb9a82509dfee
Jake Wilson <jakewilsonfl@gmail.com>
2016-12-31 13:29:02 -0500

    Adds parser.

--- parser.py
+++ parser.py
@@ -7,60 +7,110 @@ class Token:
     EOF       = 100
     ERROR     = 101
 
-def get_token(prog):
-    """ Retrieves the next token from prog, which is an open constraint file """
-    while True:
-        c = prog.read(1)
+class Parser:
 
-        if c == ' ' or c == '\t':
-            continue
-        elif c == '\n':
-            get_token.lineno += 1
-        elif is_letter(c):
-            iden = c
+    TYPE = 0
+    VAL  = 1
 
+    def __init__(self):
+        self.lineno = 1
+        self.token = ()
+
+    def parse(self, prog):
+        self.token = self.get_token(prog)
+        self.prog = prog
+        self.con_list()
+        return True
+
+    def con_list(self):
+        # TODO
+        self.con()
+
+    def con(self):
+        self.match(Token.ID)
+        self.op()
+        self.right_side()
+
+    def op(self):
+        if self.token[self.TYPE] == Token.EQUAL:
+            self.match(Token.EQUAL)
+        else: # self.token[self.TYPE] == Token.NOT_EQUAL
+            self.match(Token.NOT_EQUAL)
+
+    def right_side(self):
+        t = self.token[self.TYPE]
+        if t == Token.ID:
+            self.match(Token.ID)
+        elif t == Token.STRING:
+            self.match(Token.STRING)
+        else: # t == Token.NUM
+            self.match(Token.NUM)
+    
+    def match(self, token_type=Token.ERROR):
+        if token_type == self.token[self.TYPE]:
+            # may have to do more here
+            self.token = self.get_token()
+        else:
+            # TODO
+            #print "[Parser] Line ", self.lineno, " saw ", self.token[self.VAL], " expected ", token_type
+            raise SyntaxError()
+    
+    def get_token(self, prog=None):
+        """ Retrieves the next token from prog, which is an open constraint file """
+        if not prog:
+            prog = self.prog
+
+        while True:
             c = prog.read(1)
-            # id's must start with a letter, but can contain both letters and digits
-            while (is_letter(c) or is_digit(c)):
-                iden += c
+    
+            if c == ' ' or c == '\t':
+                continue
+            elif c == '\n':
+                self.lineno += 1
+            elif is_letter(c):
+                iden = c
+    
                 c = prog.read(1)
-
-            prog.seek(-1, 1)
-            return (Token.ID, iden)
-        elif is_digit(c):
-            num = c
-        
-            c = prog.read(1)
-            while (is_digit(c)):
-                num += c
+                # id's must start with a letter, but can contain both letters and digits
+                while (is_letter(c) or is_digit(c)):
+                    iden += c
+                    c = prog.read(1)
+    
+                prog.seek(-1, 1)
+                return (Token.ID, iden)
+            elif is_digit(c):
+                num = c
+            
                 c = prog.read(1)
-
-            prog.seek(-1, 1)
-            return (Token.NUM, int(num))
-        elif c == '!':
-            # a '=' must follow
-            if prog.read(1) != '=':
-                return (Token.ERROR,)
-            else:
-                return (Token.NOT_EQUAL,)
-        elif c == '=':
-            return (Token.EQUAL,)
-        elif c == '\'': # string
-            string = ''
-            c = prog.read(1)
-            while c != '\'':
-                if c == '':
+                while (is_digit(c)):
+                    num += c
+                    c = prog.read(1)
+    
+                prog.seek(-1, 1)
+                return (Token.NUM, int(num))
+            elif c == '!':
+                # a '=' must follow
+                if prog.read(1) != '=':
                     return (Token.ERROR,)
-                string += c
+                else:
+                    return (Token.NOT_EQUAL,)
+            elif c == '=':
+                return (Token.EQUAL,)
+            elif c == '\'': # string
+                string = ''
                 c = prog.read(1)
-
-            return (Token.STRING, string)
-        elif c == '': # EOF?
-            return (Token.EOF,)
-            break
-                
-get_token.lineno = 1
-
+                while c != '\'':
+                    if c == '':
+                        return (Token.ERROR,)
+                    string += c
+                    c = prog.read(1)
+    
+                return (Token.STRING, string)
+            elif c == '': # EOF?
+                return (Token.EOF,)
+                break
+                    
+    
 def is_letter(c):
     if c == '': return False
     char = ord(c)
@@ -70,3 +120,9 @@ def is_digit(c):
     if c == '': return False
     char = ord(c)
     return (char >= 48 and char <= 57)
+
+if __name__ == '__main__':
+    parser = Parser()
+    with open('constraints') as f:
+        parser.parse(f)
+    print parser.token
--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -13,6 +13,7 @@ class TestParser(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.f = open(TestParser.PATH, 'a+')
+        self.parser = parser.Parser()
 
     @classmethod
     def tearDownClass(self):
@@ -22,40 +23,40 @@ class TestParser(unittest.TestCase):
 
     def test_tokenizer_1(self):
         self.write('NT != SA')
-        self.assertEqual((parser.Token.ID, 'NT'), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NOT_EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.ID, 'SA'), parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'NT'), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NOT_EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'SA'), self.parser.get_token(self.f))
 
     def test_tokenizer_2(self):
         self.write('V = 25')
-        self.assertEqual((parser.Token.ID, 'V'), parser.get_token(self.f))
-        self.assertEqual((parser.Token.EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NUM, 25), parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'V'), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NUM, 25), self.parser.get_token(self.f))
 
     def test_tokenizer_3(self):
         self.write('var1')
-        self.assertEqual((parser.Token.ID, 'var1'), parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'var1'), self.parser.get_token(self.f))
 
     def test_tokenizer_4(self):
         self.write('= = !=           26 == 78')
-        self.assertEqual((parser.Token.EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NOT_EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NUM, 26), parser.get_token(self.f))
-        self.assertEqual((parser.Token.EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NUM, 78), parser.get_token(self.f))
+        self.assertEqual((parser.Token.EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NOT_EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NUM, 26), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NUM, 78), self.parser.get_token(self.f))
 
     def test_tokenizer_5(self):
         self.write('nsw != \'blue\'')
-        self.assertEqual((parser.Token.ID, 'nsw'), parser.get_token(self.f))
-        self.assertEqual((parser.Token.NOT_EQUAL,), parser.get_token(self.f))
-        self.assertEqual((parser.Token.STRING, 'blue'), parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'nsw'), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.NOT_EQUAL,), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.STRING, 'blue'), self.parser.get_token(self.f))
 
     def test_tokenizer_6(self):
         self.write('var ! \'red\'') # forgot '=' after '!'
-        self.assertEqual((parser.Token.ID, 'var'), parser.get_token(self.f))
-        self.assertEqual((parser.Token.ERROR,), parser.get_token(self.f))
+        self.assertEqual((parser.Token.ID, 'var'), self.parser.get_token(self.f))
+        self.assertEqual((parser.Token.ERROR,), self.parser.get_token(self.f))
 
     def test_is_letter(self):
         for c in xrange(ord('A'), ord('Z')):
@@ -75,6 +76,15 @@ class TestParser(unittest.TestCase):
 
         self.assertEqual(parser.is_digit(chr(ord('0') - 1)), False)
         self.assertEqual(parser.is_digit(chr(ord('9') + 1)), False)
+
+    def test_parser_1(self):
+        self.write('var = sa')
+        self.assertEqual(self.parser.parse(self.f), True)
+
+    def test_parser_2(self):
+        self.write('var == sa')
+        with self.assertRaises(SyntaxError):
+            self.parser.parse(self.f)
 
     def write(self, s):
         self.f.seek(0, 0)
